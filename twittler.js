8dbea0883e5d87905d3e894e82265aa98a62f9e4
bcbcb <bcbcb@users.noreply.github.com>
2014-10-27 21:06:03 -0700

    Clicking on username shows user's timeline

--- twittler.js
+++ twittler.js
@@ -1,32 +1,56 @@
 $(document).ready(function(){
 
   var $main = $('.main');
-  $main.html('');
-
-  var startAt = 0;
-  var endAt;
+  var tweets;  
+  var lastTweetIndex; 
+  var totalTweets;
+  var currentPage;
 
   var addNewTweets = function() {
 
-    endAt = streams.home.length;
+    totalTweets = tweets.length;
 
-    for (var i = startAt; i < endAt; i++) {
-      var tweet = streams.home[i];
+    for (var i = lastTweetIndex; i < totalTweets; i++) {
+      var tweet = tweets[i];
 
-      var $tweet = $('<div class="tweet">' + 
-        '<span class="user">@' + tweet.user +'  </span>' + 
+      var $tweet = $('<div class="tweet">' +
+        '<span class="user" data-user="' + tweet.user + '">@' + tweet.user + '</span>' + 
         '<span class="time">' + tweet.created_at + '</span>' +
         '<span class="message">' + tweet.message + i +'</span>' +
         '</div>');
 
       $main.prepend($tweet.hide());
-      $tweet.slideDown();
-      startAt++;
+      $tweet.slideDown();      
+      lastTweetIndex++;
+    }
 
+    // TODO: this updates everytime for every username element 
+    $('.user').off("click").click(function() {
+      getPage( ($(this).data('user')) ) ;
+    });
+
+  };
+
+  var getPage = function (page) {
+    if (page !== currentPage) {
+      
+      // Clear current page
+      currentPage = page;
+      $main.html('');
+      lastTweetIndex = 0; 
+
+      // Build new page of tweets
+      tweets = page === 'home' ? streams.home : streams.users[page];
+      addNewTweets();
     }
   };
 
-  addNewTweets();
-  setInterval(addNewTweets, 1000);
+  getPage('home');
+  setInterval(addNewTweets, 2000);
+
+  $('h1').click(function(){
+    getPage('home');
+  });
+
 
 });
